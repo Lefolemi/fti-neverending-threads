4264fac9f60aa05fe4a1dcfe0f6a0bb615a6120c
Lefolemi <azecklefs@gmail.com>
2026-01-29 13:32:57 +0700

    v0.0.1 - Membuat menu basic

---
+++ scripts/main_menu/utilities/version.gd
@@ -0,0 +1,8 @@
+extends Label
+
+func _ready() -> void:
+	# "application/config/version" is the path to that setting
+	var current_version = ProjectSettings.get_setting("application/config/version", "0.0.0")
+	
+	# Update the text automatically
+	text = "v" + str(current_version)
